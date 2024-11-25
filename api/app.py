42dfa55fd0cb2f45f97263267a999cb37af38b75
박성일 <112994103+Sungileo@users.noreply.github.com>
2024-11-25 16:06:03 +0900

    wtf

--- api/app.py
+++ api/app.py
@@ -1,17 +1,11 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
-# CORS 설정 추가
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+if __name__ == '__main__' : 
+    import uvicorn 
+    uvicorn.run(app)
