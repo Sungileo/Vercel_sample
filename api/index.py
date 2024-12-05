74129e40b692e3674508d0de801e5228bc88f59b
박성일 <112994103+Sungileo@users.noreply.github.com>
2024-12-05 10:09:41 +0900

    asd

--- api/index.py
+++ api/index.py
@@ -1,10 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, Request, Path
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse, RedirectResponse, JSONResponse
+from fastapi.exceptions import HTTPException
+
+
 
 app = FastAPI(
     docs_url="/docs",  # 명시적으로 docs URL 설정
     redoc_url="/redoc",  # ReDoc URL도 설정
 )
 
+# app.mount("/data", StaticFiles(directory="data"), name="data")
+templates = Jinja2Templates(directory="templates")
+
+
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root(request:Request):
+    return templates.TemplateResponse("index.html", {"request": request})
